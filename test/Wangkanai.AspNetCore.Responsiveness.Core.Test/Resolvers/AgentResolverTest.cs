0d47084a1af7d3181131b0f0839dc4fee885876f
wangkanai <sarin@wangkanai.com>
2016-08-03 00:24:00 +0700

    add agent resolver test

---
+++ test/Wangkanai.AspNetCore.Responsiveness.Core.Test/Resolvers/AgentResolverTest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2015 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wangkanai.AspNetCore.Responsiveness.Abstractions.Devices;
+
+namespace Wangkanai.AspNetCore.Responsiveness.Core.Test.Resolvers
+{
+    public class AgentResolverTest
+    {
+
+    }
+}
