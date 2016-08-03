7e7891747323e9d0b5a97b0633f478c062974156
wangkanai <sarin@wangkanai.com>
2016-08-03 18:23:11 +0700

    adding first mobile device detection test

--- test/Wangkanai.AspNetCore.Responsiveness.Core.Test/Resolvers/AgentResolverTest.cs
+++ test/Wangkanai.AspNetCore.Responsiveness.Core.Test/Resolvers/AgentResolverTest.cs
@@ -7,12 +7,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Wangkanai.AspNetCore.Responsiveness.Abstractions.Devices;
+using Wangkanai.AspNetCore.Responsiveness.Resolvers;
+using Xunit;
 
 namespace Wangkanai.AspNetCore.Responsiveness.Core.Test.Resolvers
 {
     public class AgentResolverTest
     {
+        [Fact]
+        public void Mobile_device_detection()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            var header = "User-Agent";
+            var headerValue = "Nexus 5 Build/KOT49H)";
+            context.Request.Headers.Add(header, new[] {headerValue});
 
+            var manager = new Mock<IDeviceManager>();
+            var options = new Mock<IOptions<DeviceOptions>>();
+            var resolver = new AgentResolver(options.Object, manager.Object);
+            var expected = new Mock<IDevice>();
+            expected.Setup(x => x.IsMobile).Returns(true);
+            
+            // Act
+            var device = resolver.Resolve(context);
+
+
+            // Assert
+            Assert.Equal(expected.Object.IsMobile, device.IsMobile);
+        }
     }
 }
