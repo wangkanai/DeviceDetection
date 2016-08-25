1162f6b7c5f70323bf2a16b14082583f71304f9f
wangkanai <sarin@wangkanai.com>
2016-08-25 17:57:53 +0700

    fixed device not found exception

--- src/Wangkanai.Extensions.Responsiveness/DeviceNotFoundException.cs
+++ src/Wangkanai.Extensions.Responsiveness/DeviceNotFoundException.cs
@@ -7,29 +7,9 @@ namespace Wangkanai.Extensions.Responsiveness
     [System.Runtime.InteropServices.ComVisible(true)]
     public class DeviceNotFoundException : ArgumentException, ISerializable
     {
-        private readonly string m_invalidDeviceName; // unrecognized device name
+        private string m_invalidDeviceName; // unrecognized device name
         private static string DefaultMessage => "Device Not Supported";
         public virtual string InvalidDeviceName => m_invalidDeviceName;
-
-        public DeviceNotFoundException() : base(DefaultMessage) { }
-        public DeviceNotFoundException(string message) : base(message) { }
-
-        public DeviceNotFoundException(string message, Exception innerException)
-            : base(message, innerException)
-        {            
-        }
-        public DeviceNotFoundException(string message, string invalidDeviceName, Exception innerException) :
-            base(message, innerException)
-        {
-            m_invalidDeviceName = invalidDeviceName;
-        }       
-
-        protected DeviceNotFoundException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            m_invalidDeviceName = (string)info.GetValue("InvalidDeviceName", typeof(string));
-        }
-
         public override string Message
         {
             get
@@ -45,6 +25,35 @@ namespace Wangkanai.Extensions.Responsiveness
             }
         }
 
+        public DeviceNotFoundException() : base(DefaultMessage) { }
+        public DeviceNotFoundException(string message) : base(message) { }
+
+        public DeviceNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+
+        public DeviceNotFoundException(string message, string invalidDeviceName, Exception innerException) :
+            base(message, innerException)
+        {
+            m_invalidDeviceName = invalidDeviceName;
+        }
+
+#if NET451
+        protected DeviceNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            m_invalidDeviceName = (string)info.GetValue("InvalidDeviceName", typeof(string));
+        }
+#else
+        protected DeviceNotFoundException(SerializationInfo info, StreamingContext context)
+        {
+            m_invalidDeviceName = (string) info.GetValue("InvalidDeviceName", typeof(string));
+        }
+#endif
+
+#if NET451
         [System.Security.SecurityCritical]
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
@@ -52,5 +61,11 @@ namespace Wangkanai.Extensions.Responsiveness
             base.GetObjectData(info, context);
             info.AddValue("InvalidDeviceName", m_invalidDeviceName, typeof(string));
         }
+#else
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            m_invalidDeviceName = (string)info.GetValue("InvalidDeviceName", typeof(string));
+        }
+#endif
     }
 }
