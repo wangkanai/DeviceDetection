6b98c479b8d1b99e3535e24acc8bbab7e02abc9c
Sarin Na Wangkanai <sarin@wangkanai.com>
2016-08-26 16:45:17 +0700

    add device browser

---
+++ src/Wangkanai.Extensions.Browser/DeviceBrowser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wangkanai.Extensions.Browser
+{
+    internal abstract class DeviceBrowser: IDeviceBrowser
+    {
+        
+    }
+
+    internal interface IDeviceBrowser
+    {
+        
+    }
+    internal class DesktopBrowser:DeviceBrowser
+    {
+    }
+
+    internal class TabletBrowser:DeviceBrowser
+    {
+        
+    }
+
+    internal class MobileBrowser:DeviceBrowser
+    {
+        
+    }
+    
+}
