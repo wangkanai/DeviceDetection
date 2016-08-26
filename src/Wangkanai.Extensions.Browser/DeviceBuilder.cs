b64e119e8f15249de459b572b09f3f6215029984
wangkanai <sarin@wangkanai.com>
2016-08-26 11:51:48 +0700

    add device builder

---
+++ src/Wangkanai.Extensions.Browser/DeviceBuilder.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2016 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+namespace Wangkanai.Extensions.Browser
+{
+    public class DeviceBuilder
+    {
+        public static DeviceInfo CreateDesktop() => new DeviceInfo(DeviceTypes.Desktop);
+        public static DeviceInfo CreateTablet() => new DeviceInfo(DeviceTypes.Tablet);
+        public static DeviceInfo CreateMobile() => new DeviceInfo(DeviceTypes.Mobile);                        
+    }
+}
