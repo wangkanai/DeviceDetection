0e8be535ee6c17f20e952c8658e4059b8b209989
KostaVlev <kostadinvlev@gmail.com>
2017-10-30 16:12:14 -0400

    Add tests ApplicationBuilderExtensions.cs DeviceHttpContextExtensions.cs ResponsiveBuilder.cs ResponsiveMiddleware.cs ResponsiveViewLocationExpander.cs

---
+++ test/Wangkanai.Responsive.Test/Core/DeviceHttpContextExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using Wangkanai.Detection;
+using Xunit;
+
+
+namespace Wangkanai.Responsive.Test.Core
+{
+    public class DeviceHttpContextExtensionsTests
+    {
+        [Fact]
+        public void SetDevice_HttpContext_UserPerference_ReturnsExpected()
+        {
+            var context = new DefaultHttpContext();
+            var preference = new UserPerference();
+            string responsiveContextKey = "Responsive"; // May be we can make this constant public for testing purposes.
+
+            context.SetDevice(preference);
+
+            Assert.True(context.Items.ContainsKey(responsiveContextKey));
+            Assert.Same(preference, context.Items[responsiveContextKey]);
+        }
+
+        [Fact]
+        public void SetDevice_Null_UserPerference_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((HttpContext)null).SetDevice(new UserPerference()));
+        }
+
+        [Fact]
+        public void SetDevice_HttpContext_Null_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DefaultHttpContext().SetDevice(null));
+        }
+
+        [Fact]
+        public void GetDevice_HttpContext_ReturnsExpected()
+        {           
+            string device = DeviceType.Tablet.ToString();
+            var preference = new UserPerference() { Device = device };
+            var context = new DefaultHttpContext();
+            context.SetDevice(preference);
+
+            Assert.Same(preference, context.GetDevice());
+            Assert.Same(preference.Device, context.GetDevice().Device);
+        }
+
+        [Fact]
+        public void SetDevice_Null_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((HttpContext)null).GetDevice());
+        }
+
+        [Fact]
+        public void SetDevice_InvalidContext_ReturnsNull()
+        {
+            var context = new DefaultHttpContext();
+
+            // If the device is not found it just returns NULL. May be we can throw some exception here.
+            Assert.Null(context.GetDevice());
+        }
+    }
+}
---
+++ test/Wangkanai.Responsive.Test/Core/ResponsiveViewLocationExpanderTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Wangkanai.Detection;
+using Xunit;
+
+namespace Wangkanai.Responsive.Test.Core
+{
+    public class ResponsiveViewLocationExpanderTests
+    {
+        [Fact]
+        public void Ctor_Default_Success()
+        {
+            var locationExpander = new ResponsiveViewLocationExpander();
+            // May be we can make "_format" public so we can test the default value is set properly.
+        }
+
+        [Fact]
+        public void Ctor_ResponsiveViewLocationFormat_Success()
+        {
+            var locationExpander = new ResponsiveViewLocationExpander(ResponsiveViewLocationFormat.Subfolder);
+            // May be we can make "_format" public so we can test the value is set properly.
+        }
+
+        [Fact(Skip = "This case is not handled.")]
+        public void Ctor_InvalidFormat_InvalidEnumArgumentException()
+        {
+            Assert.Throws<InvalidEnumArgumentException>(() => new ResponsiveViewLocationExpander((ResponsiveViewLocationFormat)int.MaxValue));
+        }
+
+        [Fact(Skip = "Fails with NullReferenceExeption because context.Values is not set to instance of an object.")]
+        public void PopulateValues_ViewLocationExpanderContext_Success()
+        {
+            string deviceKey = "device"; // May this one can be public in ResponsiveViewLocationExpander.cs.
+            var context = SetupViewLocationExpanderContext();
+            var locationExpander = new ResponsiveViewLocationExpander();
+            locationExpander.PopulateValues(context);
+
+            Assert.NotEqual(0, context.Values.Count);
+            Assert.Same(context.ActionContext.HttpContext.GetDevice().Device, context.Values[deviceKey]);
+        }        
+
+        [Fact]
+        public void PopulateValues_Null_ThrowsArgumentNullException()
+        {
+            var locationExpander = new ResponsiveViewLocationExpander();
+            Assert.Throws<ArgumentNullException>(() => locationExpander.PopulateValues(null));
+        }
+
+        [Fact(Skip = "Fails with NullReferenceExeption because context.Values is not set to instance of an object.")]
+        public void ExpandViewLocations_ViewLocationExpanderContext_IEnumerable_ReturnsExpected()
+        {
+            var context = SetupViewLocationExpanderContext();
+            var viewLocations = new List<string>() { "{0} location1", "{0} location2" };
+            var locationExpander = new ResponsiveViewLocationExpander();
+            locationExpander.PopulateValues(context);
+            var resultLocations = locationExpander.ExpandViewLocations(context, viewLocations);
+            
+            Assert.True(viewLocations.Count < resultLocations.ToList().Count);
+        }
+
+        [Fact(Skip = "Fails with NullReferenceExeption because context.Values is not set to instance of an object.")]
+        public void ExpandViewLocations_NoDevice_ReturnsExpected()
+        {
+            var context = SetupViewLocationExpanderContext();
+            var viewLocations = new List<string>() { "{0} location1", "{0} location2" };
+            var locationExpander = new ResponsiveViewLocationExpander();
+            var resultLocations = locationExpander.ExpandViewLocations(context, viewLocations);
+            
+            Assert.Equal(viewLocations.Count, resultLocations.ToList().Count);
+        }
+
+        [Fact]
+        public void ExpandViewLocations_Null_IEnumerable_ThrowsArgumentNullException()
+        {
+            var locationExpander = new ResponsiveViewLocationExpander();
+            Assert.Throws<ArgumentNullException>(() => locationExpander.ExpandViewLocations(null, new List<string>()));
+        }
+
+        [Fact]
+        public void ExpandViewLocations_ViewLocationExpanderContext_Null_ThrowsArgumentNullException()
+        {
+            var locationExpander = new ResponsiveViewLocationExpander();
+            Assert.Throws<ArgumentNullException>(() => locationExpander.ExpandViewLocations(SetupViewLocationExpanderContext(), null));
+        }
+
+        private ViewLocationExpanderContext SetupViewLocationExpanderContext()
+        {
+            var context = new ViewLocationExpanderContext(new ActionContext(), "View", "Controller", "Area", "Page", true);
+            context.ActionContext.HttpContext = new DefaultHttpContext();
+            context.ActionContext.HttpContext.SetDevice(new UserPerference() { Device = DeviceType.Tablet.ToString() });
+
+            return context;
+        }
+    }
+}
