bc187729f278d4b050c9cc7d0e26ed797d32d168
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-02 05:22:15 +0700

    #7 ResolverManager working with unit test

---
+++ test/Wangkanai.Responsive.Test/Core/ResolverManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wangkanai.Detection;
+using Xunit;
+
+namespace Wangkanai.Responsive.Test.Core
+{
+    public class ResolverManagerTests
+    {
+        [Fact]
+        public void Ctor_ResolverManager_ResponsiveOptions_Success()
+        {
+            var options = (Options.Create(new ResponsiveOptions())).Value;
+            var desktop = DeviceType.Desktop;
+            var manager = new ResolverManager(desktop, options);
+        }
+        [Fact]
+        public void TabletDefault_is_Mobile()
+        {
+            var desktop = DeviceType.Desktop;
+            var tablet = DeviceType.Tablet;
+            var mobile = DeviceType.Mobile;
+
+            var options = new ResponsiveOptions(desktop, mobile, mobile);
+            Assert.Equal(mobile, options.TabletDefault);
+            Assert.NotEqual(tablet, options.TabletDefault);
+            Assert.Equal(desktop, options.DesktopDefault);
+
+            var manager = new ResolverManager(tablet, options);
+            Assert.Equal(mobile.ToString(), manager.Device());
+        }
+    }
+}
