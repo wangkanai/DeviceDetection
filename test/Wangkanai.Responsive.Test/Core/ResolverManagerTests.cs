cb577c89240b7e91861e7232c3d78c55c851f31b
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-15 16:44:03 +0700

    update license

--- test/Wangkanai.Responsive.Test/Core/ResolverManagerTests.cs
+++ test/Wangkanai.Responsive.Test/Core/ResolverManagerTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Options;
+﻿// Copyright (c) 2016 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Text;
